aec454755be7a6ce079d5a002db9fe5887aca731
Marieezouaa <88006867+Marieezouaa@users.noreply.github.com>
2024-09-19 18:59:17 -0400

    Update main.dart

--- assignment/lib/main.dart
+++ assignment/lib/main.dart
@@ -10,18 +10,22 @@ class DigitalPetApp extends StatefulWidget {
   const DigitalPetApp({super.key});
 
   @override
-  _DigitalPetAppState createState() => _DigitalPetAppState();
+  State<DigitalPetApp> createState() {
+    return _DigitalPetAppState();
+  }
 }
 
 class _DigitalPetAppState extends State<DigitalPetApp> {
   String petName = "Pickels";
   int happinessLevel = 50;
   int hungerLevel = 50;
+  String mood = "Neutral";
 // Function to increase happiness and update hunger when playing with the pet
   void _playWithPet() {
     setState(() {
       happinessLevel = (happinessLevel + 10).clamp(0, 100);
       _updateHunger();
+      _updateMood();
     });
   }
 
@@ -30,6 +34,7 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
     setState(() {
       hungerLevel = (hungerLevel - 10).clamp(0, 100);
       _updateHappiness();
+      _updateMood();
     });
   }
 
@@ -40,6 +45,7 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
     } else {
       happinessLevel = (happinessLevel + 10).clamp(0, 100);
     }
+    _updateMood();
   }
 
 // Increase hunger level slightly when playing with the pet
@@ -49,6 +55,18 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
       hungerLevel = 100;
       happinessLevel = (happinessLevel - 20).clamp(0, 100);
     }
+  }
+
+  void _updateMood() {
+    setState(() {
+      if (happinessLevel > 70) {
+        mood = "Happy";
+      } else if (happinessLevel >= 30 && happinessLevel <= 70) {
+        mood = "Neutral";
+      } else {
+        mood = "Unhappy";
+      }
+    });
   }
 
   @override
@@ -62,7 +80,7 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Name: $petName',
+              '$petName is $mood',
               style: TextStyle(fontSize: 20.0),
             ),
 
